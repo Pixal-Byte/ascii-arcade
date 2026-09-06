f8e77d18174bdbe670b8a69a1e376e4c77b5eaa2
Nova <astrobyte@pixal.ro>
2026-09-06 14:25:42 +0300

    Adjust layout ratios for upper section and remove unused lower section layout

--- src/layout_constructor.py
+++ src/layout_constructor.py
@@ -5,15 +5,10 @@ from panels import header_construct
 
 root_layout = Layout()
 root_layout.split_column(
-    Layout(name="Upper", ratio=15),
+    Layout(name="Upper", ratio=16),
     Layout(name="Lower")
 )
 root_layout["Upper"].split_column(
     Layout(renderable=header_construct(),name="header"),
     Layout(name="main_window", ratio=10)
-)
-
-root_layout["Lower"].split_row(
-    Layout(name="left"),
-    Layout(name="right")
 )
