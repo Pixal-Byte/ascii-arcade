1acd82c9e60429aa6daebe8039100ffed7ae961c
Nova <astrobyte@pixal.ro>
2026-09-06 14:23:05 +0300

    Refactor console initialization and remove unused live rendering logic

--- src/const.py
+++ src/const.py
@@ -1,3 +1,5 @@
 from rich.box import ASCII as game_box
+from rich.console import Console
 
+cnsl = Console(color_system="256", force_interactive=True)
 money: int = 100
--- src/main.py
+++ src/main.py
@@ -4,11 +4,7 @@ from rich.live import Live
 from rich.console import Console
 
 def main():
-    cnsl = Console(color_system="256", force_interactive=True)
-    with Live(root_layout,console=cnsl, refresh_per_second=60, screen=True):
-        for i in range(20):
-            time.sleep(0.4)
-            cnsl.print(root_layout)
+    pass
 
 if __name__ == "__main__":
     main()
