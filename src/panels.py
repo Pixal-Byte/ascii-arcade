2c204e6cebe1de27f82e2656522150758baeb2c9
Nova <astrobyte@pixal.ro>
2026-09-18 22:54:15 +0300

    Refactor header_construct for improved readability and formatting consistency

--- src/panels.py
+++ src/panels.py
@@ -5,13 +5,34 @@ from const import money, game_box
 
 def header_construct() -> Table:
     #content
-    formatted_money = Text(text=str(money), style="Bold White", justify="center")
-    formatted_title = Text(text="gametitle", style="white", justify="left")
+    formatted_money = Text(
+        text=str(money),
+        style="Bold White",
+        justify="center"
+    )
+    formatted_title = Text(
+        text="gametitle", 
+        style="white", 
+        justify="left"
+    )
 
     #panels
-    money_bar = Panel(renderable=formatted_money, box=game_box, border_style="bold green", title="Money", style="white")
-    spacer = Panel(renderable="", box=game_box, border_style="dim white")
-    title_bar = Panel(renderable=formatted_title, box=game_box, border_style="bold")
+    money_bar = Panel(
+        renderable=formatted_money, 
+        box=game_box, border_style="bold green", 
+        title="Money", 
+        style="white"
+    )
+    spacer = Panel(
+        renderable="", 
+        box=game_box, 
+        border_style="dim white"
+    )
+    title_bar = Panel(
+        renderable=formatted_title, 
+        box=game_box, 
+        border_style="bold"
+    )
 
     #grid
     header_grid = Table.grid(expand=True)
@@ -22,4 +43,4 @@ def header_construct() -> Table:
     return header_grid
 
 def footer_construct() -> None:
-    pass #To be implemented
+    pass # To be implemented
