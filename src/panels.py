c48479dbfe4bb2b147caa555290a2cb7a3dc079e
Nova <astrobyte@pixal.ro>
2026-09-18 16:24:13 +0300

    Refactor header construction to use formatted text for improved styling and clarity

--- src/panels.py
+++ src/panels.py
@@ -1,15 +1,25 @@
 from rich.panel import Panel
 from rich.table import Table
+from rich.text import Text
 from const import money, game_box
 
 def header_construct() -> Table:
-    money_bar = Panel(renderable=str(money), box=game_box, border_style="bold green", title="Money")
-    spacer = Panel(renderable="", box=game_box, border_style="dim")
-    title_bar = Panel(renderable="gametitle", box=game_box, border_style="bold")
+    #content
+    formatted_money = Text(text=str(money), style="Bold White", justify="center")
+    formatted_title = Text(text="gametitle", style="white", justify="left")
 
+    #panels
+    money_bar = Panel(renderable=formatted_money, box=game_box, border_style="bold green", title="Money", style="white")
+    spacer = Panel(renderable="", box=game_box, border_style="dim white")
+    title_bar = Panel(renderable=formatted_title, box=game_box, border_style="bold")
+
+    #grid
     header_grid = Table.grid(expand=True)
     header_grid.add_column(ratio=1, justify="left")
     header_grid.add_column(ratio=1, justify="center")
     header_grid.add_column(ratio=1, justify="right")
     header_grid.add_row(money_bar, spacer, title_bar)
     return header_grid
+
+def footer_construct() -> None:
+    pass #To be implemented
