1b1ad1be693599e4d90359930e3c7cbe745e8741
Nova <astrobyte@pixal.ro>
2026-09-19 09:58:32 +0300

    Inline main function logic for improved readability

--- src/main.py
+++ src/main.py
@@ -3,16 +3,13 @@ from layout_constructor import root_layout
 from rich.live import Live
 from const import cnsl
 
-def main():
-    with Live(root_layout, console=cnsl, screen=True, refresh_per_second=24) as live:
-        loop_ctrl: bool = True
-        while loop_ctrl:
-            try:
-                time.sleep(0.2)
-            except KeyboardInterrupt:
-                loop_ctrl = False
-
 if __name__ == "__main__":
-    main()
+    with Live(root_layout, console=cnsl, screen=True, refresh_per_second=24) as live:
+            loop_ctrl: bool = True
+            while loop_ctrl:
+                try:
+                    time.sleep(0.2)
+                except KeyboardInterrupt:
+                    loop_ctrl = False
 
 
